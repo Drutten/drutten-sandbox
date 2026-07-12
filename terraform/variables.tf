bae985e5f7ab50c84ef625891a9b69721b00e848
Rosi Drott Kohmareh <rosi.drott@gmail.com>
2026-07-12 23:31:03 +0200

    fix: tofu fmt

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -17,11 +17,11 @@ variable "docker_image_path" {
 variable "services" {
   description = "Map of Cloud Run services to deploy"
   type = map(object({
-    image_name          = string
-    cpu                 = optional(string, "1")
-    memory              = optional(string, "512Mi")
-    max_instances       = optional(number, 10)
-    min_instances       = optional(number, 0)
+    image_name            = string
+    cpu                   = optional(string, "1")
+    memory                = optional(string, "512Mi")
+    max_instances         = optional(number, 10)
+    min_instances         = optional(number, 0)
     allow_unauthenticated = optional(bool, true)
   }))
   default = {}
